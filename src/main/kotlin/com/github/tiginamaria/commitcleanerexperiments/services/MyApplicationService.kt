28015b4cf81271bc76cfc9ab7de4215a497da2dc
GitHub Action <action@github.com>
2022-07-12 17:06:05 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/tiginamaria/commitcleanerexperiments/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tiginamaria.commitcleanerexperiments.services
+
+import com.github.tiginamaria.commitcleanerexperiments.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
